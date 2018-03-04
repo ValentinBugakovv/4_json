ff96085f7f87f3ccb6a5e922d08de785e05ac271
ValentinBugakovv <33461703+ValentinBugakovv@users.noreply.github.com>
2018-03-05 01:10:06 +0300

    ver 0.2

--- pprint_json.py
+++ pprint_json.py
@@ -1,24 +1,17 @@
-import os
 import json
 
 
-
-
 def load_data(filepath):
-    filepath = os.path.abspath(filepath)
-    with open(filepath, 'r', encoding="UTF-8") as f:
-        return json.load(f)
+    with open(filepath, 'r', encoding="UTF-8") as path:
+        return json.load(path)
 
 
-def pretty_print_json(your_json, sort=True, indents=4):
-    if type(your_json) is str:
-        print(json.dumps(json.loads(your_json), sort_keys=sort, indent=indents, ))
-    else:
-        print(json.dumps(your_json, sort_keys=sort, indent=indents))
-    return None
+def pretty_print_json(load_data):
+    print(json.dumps(load_data(input()), sort_keys=True,
+                     indent=4, ensure_ascii=False, separators=(',', ': ')))
 
 
-if __name__ == '__main__':
-    pretty_print_json(load_data(input()))
+if __name__ == "__main__":
+    pretty_print_json(load_data)
 
 
