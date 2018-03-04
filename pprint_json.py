1f710759ecc8f709a95f620beec105f44dce0048
ValentinBugakovv <33461703+ValentinBugakovv@users.noreply.github.com>
2018-03-04 22:50:02 +0300

    ver 0.2
    - Function name is changed
    - del str

--- pprint_json.py
+++ pprint_json.py
@@ -10,15 +10,15 @@ def load_data(filepath):
         return json.load(f)
 
 
-def pretty_print_json(data, sort=True, indents=4):
-    if type(data) is str:
-        print(json.dumps(json.loads(data), sort_keys=sort, indent=indents, ))
+def pretty_print_json(your_json, sort=True, indents=4):
+    if type(your_json) is str:
+        print(json.dumps(json.loads(your_json), sort_keys=sort, indent=indents, ))
     else:
-        print(json.dumps(data, sort_keys=sort, indent=indents))
+        print(json.dumps(your_json, sort_keys=sort, indent=indents))
     return None
 
 
 if __name__ == '__main__':
-    pretty_print_json(load_data(str(input())))
+    pretty_print_json(load_data(input()))
 
 
