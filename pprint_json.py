90134bdb725b5de1afdc78ddbdd7c84b83a5ceac
ValentinBugakovv <33461703+ValentinBugakovv@users.noreply.github.com>
2018-03-05 01:25:43 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -6,12 +6,13 @@ def load_data(filepath):
         return json.load(path)
 
 
-def pretty_print_json(load_data):
-    print(json.dumps(load_data(input()), sort_keys=True,
+def pretty_print_json(test):
+    print(json.dumps(test, sort_keys=True,
                      indent=4, ensure_ascii=False, separators=(",", ": ")))
 
 
 if __name__ == "__main__":
-    pretty_print_json(load_data)
+    pretty_print_json(load_data(input()))
+
 
 
