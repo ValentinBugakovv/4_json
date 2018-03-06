ce0a699d6621bfdff25144da8cadb0d32102aade
ValentinBugakovv <33461703+ValentinBugakovv@users.noreply.github.com>
2018-03-07 00:08:57 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -1,9 +1,13 @@
 import json
 import argparse
+import os
 
-parser = argparse.ArgumentParser(description='Path to json file')
-parser.add_argument("filepath", type=str, help="Path to json file")
-args = parser.parse_args()
+
+def arguments():
+    parser = argparse.ArgumentParser(description='Path to json file')
+    parser.add_argument("filepath", type=str, help="Path to json file")
+    args = parser.parse_args()
+    return args.filepath
 
 
 def load_data(filepath):
@@ -11,19 +15,22 @@ def load_data(filepath):
         return json.load(path)
 
 
-def pretty_print_json(data_result):
-    print(json.dumps(data_result, sort_keys=True,
+def pretty_print_json(json_content):
+    print(json.dumps(json_content, 
+                     sort_keys=True,
                      indent=4,
                      ensure_ascii=False,
                      separators=(",", ": ")
-                     )
-          )
+    ))
+
+
+def main():
+    data_result = load_data(arguments())
+    pretty_print_json(data_result)
 
 
 if __name__ == "__main__":
-    data_result = load_data(args.filepath)
-    pretty_print_json(data_result)
-
+    main()
 
 
 
