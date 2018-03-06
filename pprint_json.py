73abce040405ed47c93362f27381b2b91f96bb0b
ValentinBugakovv <33461703+ValentinBugakovv@users.noreply.github.com>
2018-03-07 01:45:54 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -3,16 +3,18 @@ import argparse
 import os
 
 
-def arguments():
-    parser = argparse.ArgumentParser(description='Path to json file')
-    parser.add_argument("filepath", type=str, help="Path to json file")
+def user_arguments():
+    parser = argparse.ArgumentParser(description="Path to json file")
+    parser.add_argument("filepath",
+                        help="Path to json file"
+                        )
     args = parser.parse_args()
     return args.filepath
 
 
 def load_data(filepath):
-    with open(filepath, "r", encoding="UTF-8") as path:
-        return json.load(path)
+    with open(filepath, "r", encoding="UTF-8") as json_file:
+        return json.load(json_file)
 
 
 def pretty_print_json(json_content):
@@ -25,10 +27,10 @@ def pretty_print_json(json_content):
 
 
 def main():
-    fact = arguments()
-    if fact.endswith('.json') and os.path.isfile(fact) == True:
-        data_result = load_data(fact)
-        pretty_print_json(data_result)
+    fact = user_arguments()
+    if fact.endswith(".json") and os.path.isfile(fact) == True:
+        json_result = load_data(fact)
+        pretty_print_json(json_result)
     else:
         raise ("This is not a json file")
 
