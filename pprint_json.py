7e4832acff9897440cf5b1d0b919c9e08241049f
ValentinBugakovv <33461703+ValentinBugakovv@users.noreply.github.com>
2018-03-09 02:45:37 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -3,36 +3,40 @@ import argparse
 
 
 def get_arguments():
-    parser = argparse.ArgumentParser(description="Path to json file")
-    parser.add_argument("filepath",
-                        help="Path to json file",
-                        )
+    parser = argparse.ArgumentParser(
+        description="Path to json file"
+    )
+    parser.add_argument(
+        "filepath",
+        help="Path to json file",
+    )
     args = parser.parse_args()
     return args
 
 
 def load_data(filepath):
     with open(filepath, "r", encoding="UTF-8") as json_file:
-        try:
-            return json.load(json_file)
-        except ValueError:
-            print("This is not a json file")
+        return json.load(json_file)
 
 
 def pretty_print_json(json_content):
-    print(json.dumps(json_content,
-                     sort_keys=True,
-                     indent=4,
-                     ensure_ascii=False,
-                     separators=(",", ": ")
+    print(json.dumps(
+        json_content,
+        sort_keys=True,
+        indent=4,
+        ensure_ascii=False,
+        separators=(",", ": ")
     ))
 
 
 def main():
-    user_path = get_arguments().filepath
-    list_result = load_data(user_path)
-    pretty_print_json(list_result)
+    file_path = get_arguments().filepath
+    data_result = load_data(file_path)
+    pretty_print_json(data_result)
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except ValueError:
+        print("This is not a json file")
